b528cf9558e2a20d8d9feea3c7b5f51013052c2c
Amitgit-hub <dhiman1002451@gmail.com>
2024-07-21 18:05:45 +0530

    streamlab project

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homescreen from './pages/Homescreen';
+import Navbar from './components/navigation/Navbar';
+import { navScroll } from './helper';
+import Details from './pages/Details';
+import Browse from './pages/Browse';
+import BrowseByGenre from './pages/BrowseByGenre';
+import Search from './pages/Search';
 
 function App() {
+  useEffect(() => {
+    navScroll();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Homescreen />}></Route>
+        <Route path='/details/:platform/:id' element={<Details />}></Route>
+        <Route path='/browse/:platform' element={<Browse />}></Route>
+        <Route path='/browsebygenre/:platform/:genreid' element={<BrowseByGenre />}></Route>
+        <Route path='/search' element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
